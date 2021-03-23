955e472c80e21c6e6077ed37a4040df47362c911
Mike Ganbold <munkhbayarg@google.com>
2021-03-23 11:36:48 -0700

    chore: disabling datalabeling tests due to service limited exception

--- samples/snippets/import_data_test.py
+++ samples/snippets/import_data_test.py
@@ -39,8 +39,7 @@ def dataset():
     # tear down
     testing_lib.delete_dataset(dataset.name)
 
-
-@pytest.mark.flaky(max_runs=3, min_passes=1)
+@pytest.mark.skip(reason="service is limited due to covid")
 def test_import_data(capsys, dataset):
     @backoff.on_exception(
         backoff.expo, ServerError, max_time=testing_lib.RETRY_DEADLINE
